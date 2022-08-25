2fd4ebd13085824654e813154e0951d77236fc6d
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-08-25 11:18:46 +0530

    navbar created

---
+++ realstateapp/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav className='navbar'>
+          <h1>
+            Real State Application 
+          </h1>
+         <h2>
+          Favorites
+         </h2>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
